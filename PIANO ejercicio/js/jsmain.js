5ae6c5af8c3cd908835f0469dc79130185478248
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-11 20:50:30 +0200

    2

--- PIANO ejercicio/js/jsmain.js
+++ PIANO ejercicio/js/jsmain.js
@@ -3,3 +3,28 @@
 //2 eventos 
 
 // alert ('Bienvenido a mi piano');
+
+//vamos asignar evento a un elemento
+
+const buttonDo = document.querySelector(".js-do");
+
+
+
+/* buttonDo.addEventListener('click', function () {
+    alert('Esta es la tecla DO');
+} )   ------- me lia */  
+//pruebo ahora más como en CLase: 
+
+/* function handleBoton () {
+   alert ('holasoy  la tecla DO')
+}
+buttonDo.addEventListener ('click', handleBoton ); /*/
+
+buttonDo.addEventListener ('click', reproduceSound);
+
+function reproduceSound () {
+    const buttonDoWaw = document.querySelector('.js-DWaw') ;
+    buttonDoWaw.play();
+
+}
+
