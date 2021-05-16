7be8fc3db797f7acad700991abcbe5ceff15747b
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-16 15:36:39 +0200

    fix ev intermedia

--- evaluacion intermedia repaso/jsmain.js
+++ evaluacion intermedia repaso/jsmain.js
@@ -1,12 +1,42 @@
-'use strict'
-
-/*
-Proceso 1
-enviar mensaje al usuario
-
-Proceso 2
-contar el nº de intentos
-
-
-
-*/
+'use strict';
+/*Generando el número random*/
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+​
+  const randomNumber = getRandomNumber(100); 
+  console.log("El número random es", randomNumber);
+/* hasta aquí*/
+​
+const button = document.querySelector(".js-button");
+const clue = document.querySelector ('.js-cl');
+const attemps = document.querySelector ('.js-att');
+const userNumber = document.querySelector ('.js-user_number'); 
+​
+const compare = () => {
+    const userNumber2 = userNumber.value;
+    const userNumbervalue = parseInt (userNumber2);
+    console.log (userNumbervalue);
+    if (userNumbervalue === randomNumber) {
+       clue.innerHTML = 'Has ganado'; 
+    }else if (userNumber2 < 0 || userNumber2 > 100 ) {
+        clue.innerHTML = `El número tiene que ser mayor que cero y menor que cien`; 
+    } else if (userNumber2 < randomNumber) {
+        clue.innerHTML = `El número es mayor que ${userNumber2}`;
+    }else if (userNumber2 > randomNumber) {
+        clue.innerHTML = `El número es menor que ${userNumber2}`;
+    }
+}
+button.addEventListener("click", compare);
+​
+/*contador finiquitao*/
+​
+const userCounter = document.querySelector(".js-att");
+let counter = 0;
+function countTries() {
+  counter += 1;
+  //console.log(`${counter}`);
+  userCounter.innerHTML = `Número de intentos ${counter}`;
+}
+​
+button.addEventListener("click", countTries);
